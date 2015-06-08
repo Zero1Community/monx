df3cbc5a1e692735b4eb56b88767126134da94fc
tuwid <artur@zero1.al>
2015-06-08 03:57:20 +0200

    working on algo..

--- pooler/main.js
+++ pooler/main.js
@@ -1,6 +1,9 @@
 var configs = require('../config/configs.js');
 // libs and shit 
 var amqp = require('amqplib');
+
+// kjo eventualisht do ndrrohet ne dicka me te hajrit
+var db = require('mongojs').connect('noprod', ['notifications']);
 //var API_POST_URL = "http://monx.zero1.al:31416/api/service_data";
 
 // per tu shtu:
@@ -70,17 +73,96 @@ function postToAPI(data){
 	});
 }
 
+function getLastNotific(service_id,callback){
+	var mycollection = db.collection('notifications');
+	console.log("U mor id si parameter : " + service_id);
+	//db.notifications.find({id_:service_id}).sort().limit( 1 ).toArray(function(err, recs) {
+	mycollection.find({service_id : service_id}).sort({created_at:-1}).limit(1).toArray(function(err, elem) {	
+		//console.log("U bo query");
+		//console.log("Mbarou bo query");
+		if(elem.length > 0){
+			//console.log('Statusi i fundit per id '+ service_id + " eshte "+ elem[0].status);
+			callback(null,elem[0].status);
+			//console.log(elem[0].status);
+		}
+		else{
+			console.log('Nuk kemi notifications per ate ID');
+			callback(null,-1);
+		}
+
+	});
+}
+
+
+function getLastStatus(service_id,callback){
+	var mycollection = db.collection('service_data_' + service_id);
+	console.log("U mor id si parameter : " + service_id);
+	//db.notifications.find({id_:service_id}).sort().limit( 1 ).toArray(function(err, recs) {
+	mycollection.find({}).sort({created_at:-1}).limit(1).toArray(function(err, elem) {	
+		console.log("U bo query");
+		console.log("Mbarou bo query");
+		if(elem.length > 0){
+			//console.log('Statusi i fundit per id '+ service_id + " eshte "+ elem[0].status);
+			callback(null,elem[0].status);
+			//console.log(elem[0].status);
+		}
+		else{
+			console.log('nuk kemi te dhena per ate ID');
+			callback("nuk kemi te dhena per ate ID",-1);
+		}
+	});
+}
+
 // blacklist module
 function monxBlacklist(blacklistObject){
 	var checkRBL = require('../modules/checkBlacklist.js');
 
-	checkRBL(blacklistObject.host, function(results){
+	checkRBL(blacklistObject.host, function(totalResults){
 		if(configs.debug) console.log('Scan finished for: ' + blacklistObject.host);
 		//if(configs.debug) console.log('Result: ', results);
-
+		var cleanStatus = {};
 		//The logic for the status to be handled
+		for (var i = totalResults.length - 1; i >= 0; i--) {
+			if(totalResults[i]['status'] > 0){
+				console.log(totalResults[i]);
+				cleanStatus.push({totalResults[i]})
+			}
+		};
+		////// duhet fut nej gjo ne redis ne mes 
+		////// se u cam duke bo query ne db..
+		////// minimalisht statuset e fundit te sherbimeve (paralelisht me updatin e tyre ne db)
+		/// qe te kontrollojme ne redis statuset e fundit te db-ve 
+
+		// CURR_STATUS = statusi qe na vjen nga monitorimi
+		// LAST_STATUS = statusi i fundit i sherbimit me ID ne collectionin e tij
+		// A = NQS statusi i fundit eshte CLEAN
+		// B = madhesia e objektit cleanStatus = 0
+		// C = useri eshte notificuar per LAST_STATUS 
+		//			(dmth statuset == ne service_data_collection dhe nontification)
+
+		// PSEUDO:
+		// NQS A DHE B dhe C 
+		//		ska nevoje per gje
+		// NQS A dhe !B:
+		// 		NQS C:
+		// 			NQS CURR_STATUS === LAST_STATUS
+		//				ska nevoje per gje
+		//			PERNDRYSHE 
+		//				//status problem por qe problem i ri { nga 3 bl u ben 4})
+		//				co email me problemin e ri
+		//				updato collectionin e NOTIFIC me  CURR_STATUS
+		//		PERNDRYSHE
+		//			co email		
+		//			updatojme db-ne PROBLEM
+		//if(cleanStatus.length > 0){
+		//	
+		//}
+
 		var data = {
-			message: results,
+			message: {
+				cleanStatus,
+				totalResults
+			},
 			status: 1,
 			service_id: blacklistObject._id
 		}
--- pooler/test.js
+++ pooler/test.js
@@ -1,5 +1,77 @@
-var rblcheck = require('../modules/checkBlacklist.js');
+var configs = require('../config/configs.js');
 
-rblcheck('mail.alsat.tv', function(res){
-	console.log(res);
+// po msoj doren , kjo eventualisht do ndrrohet ne dicka me te hajrit
+var db = require('mongojs').connect('mongodb://zero1:itrhzzillxqkhmbz@ds031882.mongolab.com:31882/noprod', ['service_data_556053920c3ae62c1488d102']);
+//var db = require('mongojs').connect('noprod', ['notifications']);
+
+
+function getLastNotific(service_id,callback){
+	var mycollection = db.collection('notifications');
+	console.log("U mor id si parameter : " + service_id);
+	//db.notifications.find({id_:service_id}).sort().limit( 1 ).toArray(function(err, recs) {
+	mycollection.find({service_id : service_id}).sort({created_at:-1}).limit(1).toArray(function(err, elem) {	
+		//console.log("U bo query");
+		//console.log("Mbarou bo query");
+		if(elem.length > 0){
+			//console.log('Statusi i fundit per id '+ service_id + " eshte "+ elem[0].status);
+			callback(null,elem[0].status);
+			//console.log(elem[0].status);
+		}
+		else{
+			console.log('Nuk kemi notifications per ate ID');
+			callback(null,-1);
+		}
+
+	});
+}
+
+
+function getLastStatus(service_id,callback){
+	var mycollection = db.collection('service_data_' + service_id);
+	console.log("U mor id si parameter : " + service_id);
+	//db.notifications.find({id_:service_id}).sort().limit( 1 ).toArray(function(err, recs) {
+	mycollection.find({}).sort({created_at:-1}).limit(1).toArray(function(err, elem) {	
+		console.log("U bo query");
+		console.log("Mbarou bo query");
+		if(elem.length > 0){
+			//console.log('Statusi i fundit per id '+ service_id + " eshte "+ elem[0].status);
+			callback(null,elem[0].status);
+			//console.log(elem[0].status);
+		}
+		else{
+			console.log('nuk kemi te dhena per ate ID');
+			callback("nuk kemi te dhena per ate ID",-1);
+		}
+	});
+}
+
+		// NQS madhesia e objektit cleanStatus > 0 :
+		// 		NQS i kemi cuar email robit per ket problemin
+		// 			NQS STATUSI eshte i njejte 
+		//				hic
+		//			PERNDRYSHE 
+		//				//status problem por qe problem i ri { nga 3 bl u ben 4})
+		//				co email me problemin e ri		
+		//				updatojme db-ne PROBLEM
+		//		PERNDRYSHE
+		//			co email		
+		//			updatojme db-ne PROBLEM
+
+
+
+
+
+getLastNotific('556053920c3ae62c1488d102',function(err,status){
+	if(err){
+		console.log(err);
+	}
+	else{
+		console.log(status);
+	}
 });
+//getLastStatus('11111')
+// var rblcheck = require('../modules/checkBlacklist.js');
+
+// rblcheck('mail.alsat.tv', function(res){
+// 	console.log(res);
+// });
