b2f5bf712e680684fbe8543a693222763123fdc3
tuwid <artur@zero1.al>
2015-08-11 03:53:42 +0200

    Fiksimi me bashin tani

--- gitagli.sh
+++ gitagli.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # comenti merret si parameter
+if [ "$#" -ne 1 ]
+then
+  echo "Fut me vllai nej argument (me thonjza) "
+  exit 1
+fi
+
+
+
+git checkout agli
 git add -A .
 git commit -a -m "$1"
 git push
--- gituri.sh
+++ gituri.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # comenti merret si parameter
+if [ "$#" -ne 1 ]
+then
+  echo "Fut me vllai nej argument (me thonjza) "
+  exit 1
+fi
+
 git checkout turi
 git add -A .
 git commit -a -m "$1"
