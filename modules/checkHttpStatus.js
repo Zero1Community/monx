013e89c53d4ae2e214e3bdb41abc7c5d92327889
tuwid <artur@zero1.al>
2016-02-13 16:15:34 +0100

    better error handling

--- modules/checkHttpStatus.js
+++ modules/checkHttpStatus.js
@@ -3,282 +3,204 @@ var logger   = require('../modules/logger.js')('http_status', configs.logs.http_
 var url = require('url');
 
 function checkHttpStatus(data, timeout, cb) {
+	// duke parur errno.h nga headerat e kernelit variojne nga 35 deri te 133 momentalisht 
+	// + errorcodet e http 
+	var errno_h = {
 
-  var url_data = url.parse(data.host, true);
-  //console.log(data.options);
-  var http = url_data.protocol.match(/^https(.*)/) ? require('https') : require('http');
-  var request = require('request');
+			EUSERS          :       'Too many users',
+			ENOTSOCK        :       'Socket operation on non-socket', 
+			EDESTADDRREQ    :       'Destination address required',
+			EMSGSIZE        :       'Message too long',
+			EPROTOTYPE      :       'Protocol wrong type for socket',
+			ENOPROTOOPT     :       'Protocol not available',
+			EPROTONOSUPPORT :       'Protocol not supported',
+			ESOCKTNOSUPPORT :       'Socket type not supported',
+			EOPNOTSUPP      :       'Operation not supported on transport endpoint',
+			EPFNOSUPPORT    :       'Protocol family not supported',
+			EAFNOSUPPORT    :       'Address family not supported by protocol',
+			EADDRINUSE      :       'Address already in use',
+			EADDRNOTAVAIL   :       'Cannot assign requested address',
+			ENETDOWN        :       'Network is down',
+			ENETUNREACH     :       'Network is unreachable',
+			ENETRESET       :       'Network dropped connection because of reset',
+			ECONNABORTED    :       'Software caused connection abort',
+			ECONNRESET      :       'Connection reset by peer',
+			ENOBUFS         :       'No buffer space available',
+			EISCONN         :       'Transport endpoint is already connected',
+			ENOTCONN        :       'Transport endpoint is not connected',
+			ESHUTDOWN       :       'Cannot send after transport endpoint shutdown',
+			ETOOMANYREFS    :       'Too many references: cannot splice',
+			ETIMEDOUT       :       'Connection timed out',
+			ECONNREFUSED    :       'Connection refused',
+			EHOSTDOWN       :       'Host is down',
+			EHOSTUNREACH    :       'No route to host',
+			EALREADY        :       'Operation already in progress',
+			EINPROGRESS     :       'Operation now in progress',
+			ESTALE          :       'Stale file handle',
+			EUCLEAN         :       'Structure needs cleaning', 
+			ENOTNAM         :       'Not a XENIX named type file', 
+			ENAVAIL         :       'No XENIX semaphores available',
+			EISNAM          :       'Is a named type file',
+			EREMOTEIO       :       'Remote I/O error',
+			EDQUOT          :       'Quota exceeded',
+			ENOTFOUND			  : 			'Unable to resolve host',
+			
+			200: 'Status OK',
+			201: 'Created',
+			202: 'Accepted',
+			203: 'Non-Authoritative Information',
+			204: 'No Content',
+			205: 'Reset Content',
+			206: 'Partial Content',
+			207: 'Multi-Status',
+			208: 'Already Reported',
 
-  var options = {
-    url: data.host,
-    agent: new http.Agent({keepAlive:false}),
-  };
+			300: 'Multiple Choices',
+			301: 'Moved Permanently',
+			302: 'Found',
+			303: 'See Other',
+			304: 'Not Modified',
+			305: 'Use Proxy',
+			306: '(Unused) Redirect',
+			307: 'Temporary Redirect',
+			308: 'Permanent Redirect',
 
-  if(url_data.protocol.match(/^https(.*)/)) {
-    // error i cuditshem
-    if(data.options){
-      if(data.options['ignore_ssl_issues'])
-      options.strictSSL = false;
-      console.log('We got ignore_ssl_issues flag, setting flag ' + options.strictSSL);
-    } 
-    else{
-      options.strictSSL = true;
-    }
-  }else{
-    console.log('We didnt get ignore_ssl_issues flag ');
-  }
+			400: 'Bad Request',
+			401: 'Unauthorized',
+			402: 'Payment Required',
+			403: 'Forbidden',
+			404: 'Not Found',
+			405: 'Method Not Allowed',
+			406: 'Not Acceptable',
+			407: 'Proxy Authentication Required',
+			408: 'Request Timeout',
+			409: 'Conflict',
+			410: 'Gone',
+			411: 'Length Required',
+			412: 'Precondition Failed',
+			413: 'Payload Too Large',
+			414: 'URI Too Long',
+			415: 'Unsupported Media Type',
+			416: 'Range Not Satisfiable',
+			417: 'Expectation Failed',
+			421: 'Misdirected Request',
+			422: 'Unprocessable Entity',
+			423: 'Locked',
+			424: 'Failed Dependency',
+			426: 'Upgrade Required',
+			428: 'Precondition Required',
+			429: 'Too Many Requests',
+			431: 'Request Header Fields Too Large',
+			451: 'Unavailable for Legal Reasons',
 
-  request.get(options, function(e, res, body) {
-    if(e) {
-        //console.log(e.code);
-      if (e.code == 'ECONNRESET' || e.code == 'ECONNREFUSED') {
-        // probl firewalli
-        console.log('Connection reset/refused  / Firewall Issue ' + e.code );
-          return cb({message: 'Connection reset/refused  / Firewall Issue ', status_code: '-2', status: 'ERROR'});
-      }
-      else if (e.code == 'ENOTFOUND') {
-        // probl dns
-        console.log('Unable to resolve host  / DNS Issue ' + e.code );
-          return cb({message: 'Unable to resolve host  / DNS Issue ', status_code: '-3', status: 'ERROR'});
-      }
-      else if (e.code  == 'ETIMEDOUT') {
-        // timeout
-        console.log('Connection timeout  / Port|TCP|Host Issue ' + e.code );
-          return cb({message: 'Connection timeout  / Port|TCP|Host Issue ', status_code: '-4', status: 'ERROR'});
-      }
-      else if (e.code == 'EHOSTUNREACH') {
-        //
-        console.log('Destination host unreachable  / Network Issue ' + e.code );
-          return cb({message: 'Destination host unreachable  / Network Issue ', status_code: '-5', status: 'ERROR'});
-      }
-      else {
-        console.log('Unhandled Issue  / Issue ' + e.code );
-        return cb({message: 'Unhandled Issue  / Issue ', status_code: '-6', status: 'ERROR'});
-        // nej error i cuditshem
-        //ESOCKETTIMEDOUT,  EPIPE, EAI_AGAIN
-      }
-      //console.log(e);
-    }
-    else{
-      var messageC = '';
-      if(res.statusCode >= 600 && res.statusCode < 100){
-        // invalid
-        console.log('Invalid code ', res.statusCode);
-        return cb({message: 'Invalid code ', status_code: res.statusCode, status: 'ERROR', dump : body});
-      }
-      if(res.statusCode >= 500 && res.statusCode < 600){ // to be rechecked
-        // internal error
-        switch(res.statusCode){
-          case 500:
-            messageC = 'Internal Server Error';
-            break;
-          case 501:
-            messageC = 'Not Implemented';
-            break;
-          case 502:
-            messageC = 'Bad Gateway';
-            break;
-          case 503:
-            messageC = 'Service Unavailable';
-            break;
-          case 504:
-            messageC = 'Gateway Timeout';
-            break;
-          case 505:
-            messageC = 'HTTP Version Not Supported';
-            break;
-          case 506:
-            messageC = 'Variant Also Negotiates';
-            break;
-          case 507:
-            messageC = 'Insufficient Storage';
-            break;
-          case 508:
-            messageC = 'Loop Detected';
-            break;
-          case 509:
-            messageC = 'Unassigned';
-            break;
-          case 510:
-            messageC = 'Not Extended';
-            break;
-          case 511:
-            messageC = 'Network Authentication Required';
-            break;
-          default:
-            messageC = 'Unassigned 500 Error';
-        }
-        console.log('Not found,  Got code: ', res.statusCode);
-        return cb({message: messageC, status_code: res.statusCode, status: 'ERROR', dump : body});
-      }
+			500: 'Internal Server Error',
+			501: 'Not Implemented',
+			502: 'Bad Gateway',
+			503: 'Service Unavailable',
+			504: 'Gateway Timeout',
+			505: 'HTTP Version Not Supported',
+			506: 'Variant Also Negotiates',
+			507: 'Insufficient Storage',
+			508: 'Loop Detected',
+			509: 'Unassigned',
+			510: 'Not Extended',
+			511: 'Network Authentication Required'
+	};
 
-      if(res.statusCode >= 400 && res.statusCode < 500){
-        // not found
-        switch(res.statusCode){
-          case 400:
-            messageC = 'Bad Request';
-            break;
-          case 401:
-            messageC = 'Unauthorized';
-            break;
-          case 402:
-            messageC = 'Payment Required';
-            break;
-          case 403:
-            messageC = 'Forbidden';
-            break;
-          case 404:
-            messageC = 'Not Found';
-            break;
-          case 405:
-            messageC = 'Method Not Allowed';
-            break;
-          case 406:
-            messageC = 'Not Acceptable';
-            break;
-          case 407:
-            messageC = 'Proxy Authentication Required';
-            break;
-          case 408:
-            messageC = 'Request Timeout';
-            break;
-          case 409:
-            messageC = 'Conflict';
-            break;
-          case 410:
-            messageC = 'Gone';
-            break;
-          case 411:
-            messageageC = 'Length Required';
-            break;
-          case 412:
-            messageC = 'Precondition Failed';
-            break;
-          case 413:
-            messageC = 'Payload Too Large';
-            break;
-          case 414:
-            messageC = 'URI Too Long';
-            break;
-          case 415:
-            messageC = 'Unsupported Media Type';
-            break;
-          case 416:
-            messageC = 'Range Not Satisfiable';
-            break;
-          case 417:
-            messageC = 'Expectation Failed';
-            break;
-          case 421:
-            messageC = 'Misdirected Request';
-            break;
-          case 422:
-            messageC = 'Unprocessable Entity';
-            break;
-          case 423:
-            messageC = 'Locked';
-            break;
-          case 424:
-            messageC = 'Failed Dependency';
-            break;
-          case 426:
-            messageC = 'Upgrade Required';
-            break;
-          case 428:
-            messageC = 'Precondition Required';
-            break;
-          case 429:
-            messageC = 'Too Many Requests';
-            break;
-          case 431:
-            messageC = 'Request Header Fields Too Large';
-            break;
-          case 451:
-            messageC = 'Unavailable for Legal Reasons';
-            break;
-          default:
-            messageC = 'Unassigned 400 ERROR';
-          }
-        console.log('Not found,  Got code: ', res.statusCode);
-        return cb({message: messageC , status_code: res.statusCode, status: 'ERROR', dump : body});
-      }
+	var url_data = url.parse(data.host, true);
+	//console.log(data.options);
+	var http = url_data.protocol.match(/^https(.*)/) ? require('https') : require('http');
+	var request = require('request');
 
-      if(res.statusCode >= 300 && res.statusCode < 400){
-        // redirect
-        switch(res.statusCode){
-          case 300:
-            messageC = 'Multiple Choices';
-            break;
-          case 301:
-            messageC = 'Moved Permanently';
-            break;
-          case 302:
-            messageC = 'Found';
-            break;
-          case 303:
-            messageC = 'See Other';
-            break;
-          case 304:
-            messageC = 'Not Modified';
-            break;
-          case 305:
-            messageC = 'Use Proxy';
-            break;
-          case 306:
-            messageC = '(Unused) Redirect';
-            break;
-          case 307:
-            messageC = 'Temporary Redirect';
-            break;
-          case 308:
-            messageC = 'Permanent Redirect';
-            break;
-          default:
-            messageC = 'Unassigned 300 Error';
-        }
+	var options = {
+		url: data.host,
+		agent: new http.Agent({keepAlive:false}),
+	};
 
-        console.log(messageC + '  Got code: ',res.statusCode);
-        return cb({message: messageC, status_code: res.statusCode, status: 'ERROR', dump : body});
-      }
-      if(res.statusCode >= 200 && res.statusCode < 300){
-        // ok but not really
-        switch(res.statusCode){
-          case 200:
-            messageC = 'Status OK';
-            break;
-          case 201:
-            messageC = 'Created';
-            break;
-          case 202:
-            messageC = 'Accepted';
-            break;
-          case 203:
-            messageC = 'Non-Authoritative Information';
-            break;
-          case 204:
-            messageC = 'No Content';
-            break;
-          case 205:
-            messageC = 'Reset Content';
-            break;
-          case 206:
-            messageC = 'Partial Content';
-            break;
-          case 207:
-            messageC = 'Multi-Status';
-            break;
-          case 208:
-            messageC = 'Already Reported';
-            break;
-          default:
-            messageC = 'Unassigned 200 Error';
-        }
+	if(url_data.protocol.match(/^https(.*)/)) {
+		//TODO: review this
+		if(data.options){
+			if(data.options['ignore_ssl_issues'])
+			options.strictSSL = false;
+			logger('debug','We got ignore_ssl_issues flag, setting flag ' + options.strictSSL);
+		} 
+		else{
+			options.strictSSL = true;
+		}
+	}else{
+		logger('debug','We didnt get ignore_ssl_issues flag ');
+		}
 
-        console.log(messageC +'  Got code: ' + res.statusCode);
-        return cb({message: messageC, status_code : res.statusCode, status: 'OK'});
-      }
-    }
-  });
+	request.get(options, function(e, res, body) {
+		if(e) {
+			if (errno_h[e.code]){
+				logger('debug', errno_h[e.code] + ' ' + e.code );
+				return cb({message: errno_h[e.code] , status_code: e.code, status: 'ERROR'});
+			} 
+			else {
+				logger('debug','Connection Issue ' + e.code );
+				return cb({message: 'Connection Issue ' + e.code, status_code: e.code, status: 'ERROR'});
+			}
+		}
+		else{
+			if(res.statusCode >= 600 && res.statusCode < 100){
+				// invalid
+				logger('debug','Invalid Status Code ' + res.statusCode);
+				return cb({message: 'Invalid Status Code ', status_code: res.statusCode, status: 'ERROR', dump : body});
+			}
+			if(res.statusCode >= 500 && res.statusCode < 600){ // to be rechecked
+				// internal error
+				if(errno_h[res.statusCode]){
+					logger('debug', errno_h[res.statusCode] + '  Got code: '+ res.statusCode);
+					return cb({message: errno_h[res.statusCode] , status_code: res.statusCode, status: 'ERROR', dump : body});
+				}
+				else{
+					logger('debug','Server Error,  Got code: '+ res.statusCode);
+					return cb({message: 'Server Error,  Got code: '+ res.statusCode, status_code: res.statusCode, status: 'ERROR', dump : body});
+				}
+			}
+
+			if(res.statusCode >= 400 && res.statusCode < 500){
+				// not found
+				if(errno_h[res.statusCode]){
+					logger('debug', errno_h[res.statusCode] + '  Got code: ' + res.statusCode);
+					return cb({message: errno_h[res.statusCode] , status_code: res.statusCode, status: 'ERROR', dump : body});
+				}	
+				else{
+					logger('debug','Client-Server Error ,  Got code: ' + res.statusCode);
+					return cb({message: 'Client-Server Error ,  Got code: ' + res.statusCode , status_code: res.statusCode, status: 'ERROR', dump : body});
+				}			
+			}
+
+			if(res.statusCode >= 300 && res.statusCode < 400){
+				// redirect, we're not supposed to get these after enabling Follow Link 
+				if(errno_h[res.statusCode]){
+					logger('debug',errno_h[res.statusCode] + '  Got code: ' + res.statusCode);
+					return cb({message: errno_h[res.statusCode], status_code: res.statusCode, status: 'ERROR', dump : body});
+				}
+				else{
+					logger('debug','Redirect Error' + '  Got code: ' + res.statusCode);
+					return cb({message: 'Redirect Error' + '  Got code: ' + res.statusCode, status_code: res.statusCode, status: 'ERROR', dump : body});
+				}
+			}
+			if(res.statusCode >= 200 && res.statusCode < 300){
+				if(errno_h[res.statusCode]){
+					if(res.statusCode == 200){
+						logger('debug','Status OK ' + '  Got code: ' + res.statusCode);
+						return cb({message: 'Status OK ', status_code : res.statusCode, status: 'OK'});
+					}
+					else{
+						logger('debug', errno_h[res.statusCode] + '  Got code: ' + res.statusCode);
+						return cb({message: errno_h[res.statusCode] + '  Got code: ' + res.statusCode, status_code : res.statusCode, status: 'ERROR'});						
+					}
+				}
+				else{
+						logger('debug', 'Unexpected status code, ' + '  Got code: ' + res.statusCode);
+						return cb({message: 'Unexpected status code, ' + res.statusCode , status_code : res.statusCode, status: 'ERROR'});						
+				}
+			}
+		}
+	});
 }
 
 module.exports = checkHttpStatus;
