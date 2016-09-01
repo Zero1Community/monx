049ec908ebc63ba3e12cf3d8a57413e75179583b
tuwid <artur@zero1.al>
2016-09-01 15:55:08 +0200

    minor change

--- pooler/wP.py
+++ pooler/wP.py
@@ -190,12 +190,12 @@ channel.queue_declare(queue='service_checks', durable=False)
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-    print json.loads(body)    
-    processWork(json.loads(body))
-    time.sleep(body.count(b'.'))
-    print(" [x] Done")
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+		print(" [x] Received %r" % body)
+		print json.loads(body)    
+		processWork(json.loads(body))
+		time.sleep(body.count(b'.'))
+		print(" [x] Done")
+		ch.basic_ack(delivery_tag = method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(callback,queue='service_checks')
